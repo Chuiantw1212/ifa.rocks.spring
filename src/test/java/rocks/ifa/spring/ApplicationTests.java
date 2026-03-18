9d2bef511a813024c9ca58fa11abd0baf04d9228
EN Chu <chuiantw1212@gmail.com>
2026-03-18 20:50:06 +0800

    wip

---
+++ src/test/java/rocks/ifa/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package rocks.ifa.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
